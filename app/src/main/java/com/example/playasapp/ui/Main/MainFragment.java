104deb03c5f9827295fbd6cec9935fe4d6a90352
r34mod <zondacamarafx10@gmail.com>
2022-03-23 13:02:38 +0100

    feature: continue with project

--- app/src/main/java/com/example/playasapp/ui/Main/MainFragment.java
+++ app/src/main/java/com/example/playasapp/ui/Main/MainFragment.java
@@ -8,19 +8,26 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.*;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
+import com.example.playasapp.MainActivity;
 import com.example.playasapp.R;
 
 public class MainFragment extends Fragment {
 
-    private MainFragment mainFragment;
+    private MainViewModel mainViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanveState){
-        mainFragment = ViewModelProviders.of(this).get(MainFragment.class);
+        mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
         View root = inflater.inflate(R.layout.fragment_main, container, false);
         final TextView textView = root.findViewById(R.id.txtMain);
-
+        mainViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                textView.setText(s);
+            }
+        });
         return root;
     }
 
---
+++ app/src/main/java/com/example/playasapp/ui/Main/MainViewModel.java
@@ -0,0 +1,18 @@
+package com.example.playasapp.ui.Main;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    private MutableLiveData<String> mText;
+
+    public MainViewModel(){
+        mText = new MutableLiveData<>();
+        mText.setValue("");
+    }
+
+    public LiveData<String> getText(){
+        return mText;
+    }
+}
