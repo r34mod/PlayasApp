94814225c653bc5cba0dd88b0caab18f741d61fe
r34mod <zondacamarafx10@gmail.com>
2022-03-30 19:18:17 +0200

    feature: continue with project

---
+++ app/src/main/java/com/example/playasapp/adds/AddBeachActivity.java
@@ -0,0 +1,45 @@
+package com.example.playasapp.adds;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+import com.example.playasapp.R;
+
+public class AddBeachActivity extends AppCompatActivity {
+
+    private RatingBar ratingBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_beach);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        ratingBar = (RatingBar)findViewById(R.id.ratingBar);
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                Toast.makeText(AddBeachActivity.this, "La puntacion dada es: "+rating, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
